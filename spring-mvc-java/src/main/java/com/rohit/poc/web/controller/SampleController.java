191ca3ef5ad883b1a29636bf17cf1be74008d1f7
Rohit Dhingra <Rohit Dhingra@DESKTOP-TQ58HNN>
2019-04-27 09:27:06 +0530

    Other ways for request mapping

--- spring-mvc-java/src/main/java/com/rohit/poc/web/controller/SampleController.java
+++ spring-mvc-java/src/main/java/com/rohit/poc/web/controller/SampleController.java
@@ -3,6 +3,8 @@ package com.rohit.poc.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/spring-mvc-java")
@@ -10,5 +12,26 @@ public class SampleController {
 	@GetMapping("/sample")
 	public String showForm() {
 		return "sample";
+	}
+	
+	@RequestMapping(value="/ex/foos",headers={"key1=val1","key2=val2"},method=RequestMethod.GET	)
+	@ResponseBody
+	public String getFoosBySimplePath()
+	{
+		return "Get some Foos with headers";
+	}
+	
+	@RequestMapping(value="/ex/foos/acce",headers="Accept=application/json",
+			method=RequestMethod.GET, produces="application/json"	)
+	@ResponseBody
+	public String getFoosBySimplePathAccept()
+	{
+		return "Get some Foos with headers";
+	}
+	
+	@RequestMapping(value = "*", method = RequestMethod.GET)
+	@ResponseBody
+	public String getFallback() {
+		return "Fallback for GET Requests";
 	}
 }
